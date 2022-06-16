e6c6dc91b43e8854fabe80ebacf23c82c80fb015
Andrew <9yards.to.nowhere@gmail.com>
2022-06-16 06:30:25 +0500

    Update sfi.sh

--- sfi.sh
+++ sfi.sh
@@ -12,3 +12,38 @@
 
 
 grep -rnw hosts -e ansible_host | awk -F "=" '{print $2}' | awk -F " " '{print $1}' | grep -e '172.16.*' | sort | uniq
+
+
+nodes=()
+template=()
+
+for ((i=1; i<=254; i++))
+do
+        template[i]=$i
+done
+
+echo ${template[*]}
+
+nodes=$(grep -rw $1 -e 'ansible_host' | \
+                awk -F "=" '{print $2}' | \
+                awk -F " " '{print $1}' | \
+                grep -e '172.16.233.*' | \
+                uniq | \
+                awk -F "." '{print $4}')
+
+echo ${#nodes[*]}
+printf "%s\n" "${nodes[@]}"
+#exist_nodes=$(grep -rw $1 -e 'ansible_host' | \
+#               awk -F "=" '{print $2}' | \
+#               awk -F " " '{print $1}' | \
+#               grep -e '172.16.233.*' | \
+#               uniq | \
+#               awk -F "." '{print $4}')
+#
+#echo $exist_nodes
+#IFS=' '
+#for i in $exist_nodes
+#do
+#       echo ${exist_nodes[*]}
+#       echo ${#exist_nodes[*]}
+#done
