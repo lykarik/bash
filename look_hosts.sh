13c67b0f4cdcf017430aa459ebd1ffa6234d640b
lykarik <9yards.to.nowhere@gmail.com>
2022-06-19 11:49:13 +0500

    create look_host.sh

---
+++ look_hosts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pg10a=172.16.3.37
+timer=172.16.2.72
+
+hosts=(
+"172.16.3.37 pg10a"
+"172.16.2.72 ppak-app-timer-srv01"
+)
+
+look_host(){
+IFS=";"
+for i in ${hosts[*]}
+do
+	if [[ -z $(grep /etc/hosts -e '$i') ]]
+	then
+		echo "$i - was added"
+	else
+		echo $i | awk -F " " '{print $1" "$2}' >> /etc/hosts
+	fi
+done
+}
+
+look_host
