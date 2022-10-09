a89cd6593b977ed1e73eefae2ac29f9b377f0b3b
Andrew <9yards.to.nowhere@gmail.com>
2022-10-09 22:40:30 +0500

    Update test.py

--- take_metrics/test.py
+++ take_metrics/test.py
@@ -1,24 +1,30 @@
 import subprocess
 import time
 
+print 'Taking info...'
+
 pid = int(subprocess.check_output(["./take_metrics","-pid"]))
-print(pid)
-
 fgc_start = int(subprocess.check_output(["./take_metrics","-fgc"]))
-print 'start at -', fgc_start
+print '{0:8} {1:3} {2:8} {3:3}'.format('Service PID:', pid, 'FGC count:', fgc_start)
 
-for i in range(6):
+print 'Look jstat'
+print '{0:8} {1:8} {2:8}'.format('EDEN','OLD','FGC')
+
+eden_count = str(subprocess.check_output(["./take_metrics","-eden"])).strip()
+old_count = str(subprocess.check_output(["./take_metrics","-old"])).strip()
+
+
+for i in range(1):
         fgc_count = int(subprocess.check_output(["./take_metrics","-fgc"]))
 
         if fgc_count > fgc_start:
-                print'FGC is growth -', fgc_count
-
                 fgc_diff = fgc_count - fgc_start
-                print 'FGC diff is:', fgc_diff
-        else:
-                print fgc_start, '=', fgc_count
 
+        print '{0:8} {1:8} {2:3}'.format(eden_count, old_count, fgc_count)
         time.sleep(5)
 
 print 'Done. Now FGC is:', fgc_count
 
+
+class_list = subprocess.check_output(["./take_metrics","-jmap"])
+print '\n', class_list
